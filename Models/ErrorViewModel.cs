a27a7847c5eca7003e8b7c209bc91247cb2c3de9
olaf kuras <kurasolaf@gmail.com>
2020-11-07 20:19:05 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SelfAplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
