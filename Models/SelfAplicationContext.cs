b95a5b3d99ed6c1f5980bef28135435446ecb3d5
olaf kuras <kurasolaf@gmail.com>
2020-11-08 21:22:44 +0100

    8.11

---
+++ Models/SelfAplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace SelfAplication.Models
+{
+    public class SelfAplicationContext : DbContext
+    {
+        public SelfAplicationContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SelfAplication.Models;
 
 namespace SelfAplication
 {
@@ -24,6 +26,8 @@ namespace SelfAplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<SelfAplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
