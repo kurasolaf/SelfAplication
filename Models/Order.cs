dab514be718eab1c4f25716e389e29c035be7a4a
olaf kuras <kurasolaf@gmail.com>
2020-11-11 22:16:07 +0100

    Add SelfAplication list services

--- Models/Order.cs
+++ Models/Order.cs
@@ -9,6 +9,7 @@ namespace SelfAplication.Models
     {
 
         public int Id { get; set; }
+
         public string Title { get; set; }
 
         public string Desc { get; set; }
---
+++ Services/AplicationListServices.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SelfAplication.Services
+{
+    public class OrderListServices
+    {
+        private SelfAplicationContext _context;
+
+        public OrderListServices(SelfAplicationContext context)
+        {
+            _context = context;
+
+
+        }
+
+
+
+    }
+}
