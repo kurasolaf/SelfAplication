857edf057aaa9371f5aa39fd0f6843a1e085b948
olaf kuras <kurasolaf@gmail.com>
2020-11-30 21:19:49 +0100

    Spell migration

--- Migrations/20201130201133_Spellos.Designer.cs
+++ Migrations/20201130201133_Spellos.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SelfAplication.Models;
 
 namespace SelfAplication.Migrations
 {
     [DbContext(typeof(SelfAplicationContext))]
-    partial class SelfAplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20201130201133_Spellos")]
+    partial class Spellos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20201130201133_Spellos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SelfAplication.Migrations
+{
+    public partial class Spellos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Spells",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SpellName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpellElement = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpellLvl = table.Column<int>(type: "int", nullable: false),
+                    SpellDmg = table.Column<int>(type: "int", nullable: false),
+                    SpellRange = table.Column<int>(type: "int", nullable: false),
+                    SpellCastTime = table.Column<int>(type: "int", nullable: false),
+                    SpellConcetration = table.Column<int>(type: "int", nullable: false),
+                    SpellDesc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spells", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Spells");
+        }
+    }
+}
